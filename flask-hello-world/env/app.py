5a0e73395704cf1729af50f2a2d533f1cc5393a5
Christopher Kenrick <Christopher.Kenrick@gmail.com>
2014-05-21 21:33:06 -0400

    flask updates

--- flask-hello-world/env/app.py
+++ flask-hello-world/env/app.py
@@ -1,10 +1,11 @@
-# ---- Flask Hello World ---- #
-
 # import the Flask class from the flask module
 from flask import Flask
 
 # create the application object
-app = Flask( __name__)
+app = Flask(__name__)
+
+# error handling
+app.config["DEBUG"] = True
 
 # use decorators to link the function to a url
 @app.route("/")
@@ -12,7 +13,13 @@ app = Flask( __name__)
 
 # define the view using a function, which returns a string
 def hello_world():
-  return "Hello, World!"
+  return "Hello, World!?!?!?!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+  return search_query
 
 # start the development server using the run() method
-if __name__ == "__main__": app.run()
+if __name__ == "__main__":
+  app.run()
